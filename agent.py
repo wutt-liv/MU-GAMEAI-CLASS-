5a4474ab816b29a9e264d36253722a2d83839974
wutthieak <wutthieuk@gmail.com>
2025-08-31 16:10:05 +0700

    lab-31082025-3

--- agent.py
+++ agent.py
@@ -9,8 +9,23 @@ class Agent:
         self.position = position
         self.vel = Vector2(0,0)
         self.acc = Vector2(0,0)
-        self.acc.x = 1
-        self.acc.y = 1
+        self.mass = 1.0
+    
+    def seek_to(self, target_pos):
+        MAX_FORCE = 5
+        d = target_pos - self.position
+        if d.length_squared() == 0:
+            return
+        
+        desired = d.normalize() * MAX_FORCE
+        steering = desired - self.vel 
+        if steering.length() > MAX_FORCE: 
+            steering.scale_to_length(MAX_FORCE)
+
+        self.apply_force(steering)
+
+    def apply_force(self, force):
+        self.acc += force / self.mass
     
     def update(self, delta_time_ms):
         self.vel = self.vel + self.acc
--- lab2.py
+++ lab2.py
@@ -14,15 +14,24 @@ class App:
         self.screen_height = 720
         self.screen = pygame.display.set_mode((screen_width, screen_height))
         self.clock = pygame.time.Clock()
+        self.CHAGE_DIR = pygame.USEREVENT +1
+        pygame.time.set_timer(self.CHAGE_DIR, 2000)
         self.running = True
-        self.ball = Agent(position = Vector2(screen_width/2, screen_height/2), radius= 100, color= (255,0,0))
+        self.ball = Agent(position = Vector2(screen_width/2, screen_height/2), 
+                          radius= 100, 
+                          color= (255,0,0))
+        self.target = Vector2(0, 0)
+        
     
     def handle_input(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+        mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+        self.target = Vector2(mouse_pos_x, mouse_pos_y)
     
     def update(self, delta_time_ms):
+        self.ball.seek_to(self.target)
         self.ball.update(delta_time_ms)
 
     def draw(self):
