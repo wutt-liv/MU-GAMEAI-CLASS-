12ffa229e44ffe69ae8334b7ec1dedf422e83f02
wutthieak <wutthieuk@gmail.com>
2025-09-14 16:12:49 +0700

    14092025--

--- agent.py
+++ agent.py
@@ -1,15 +1,25 @@
 
-from pygame.draw import circle
+from pygame.draw import circle, line
 from pygame.math import Vector2
 
 class Agent:
     def __init__(self, position, radius, color):
+        self.screen_width = 1280
+        self.screen_height = 720
         self.radius = radius
         self.cycle_color = color
         self.position = position
         self.vel = Vector2(0,0)
         self.acc = Vector2(0,0)
         self.mass = 1.0
+        self.EYE_SIGHT = 100
+        self.STOP_DIST = 5
+        self.detect_distance = 300
+        self.safe_distance = 500
+        self.origin_post = Vector2(self.screen_width/2,0)
+        self.first_stop = Vector2(self.screen_width, self.screen_height/2)
+        self.second_stop = Vector2(self.screen_width/2, self.screen_height)
+        
     
     def seek_to(self, target_pos):
         MAX_FORCE = 5
@@ -24,6 +34,78 @@ class Agent:
 
         self.apply_force(steering)
 
+    def arrive_to(self, target_pos):
+        '''add arriving bahavior here'''
+        MAX_FORCE = 5
+
+        d = target_pos - self.position
+
+        if d.length_squared() == 0:
+            return True
+        
+        dist = d.length() # square root is expensive in games
+        if dist < self.STOP_DIST:
+            desired = Vector2(0,0)
+
+        elif dist < self.EYE_SIGHT:
+            # slowing down forces
+            desired = d.normalize() * (MAX_FORCE*(dist/self.EYE_SIGHT))
+        else:
+            desired = d.normalize() * MAX_FORCE
+
+        steering = desired - self.vel 
+        if steering.length() > MAX_FORCE: 
+            steering.scale_to_length(MAX_FORCE)
+        
+        self.apply_force(steering)
+
+    def flee_from(self, target_pos):
+        MAX_FORCE = 5
+
+        d = target_pos - self.position
+        dist = d.length()
+        if d.length_squared() == 0:
+            return
+        
+        if dist > self.safe_distance:
+            desired = Vector2(0,0)
+        elif dist < self.detect_distance:
+            desired = d.normalize() * (MAX_FORCE*(self.detect_distance/dist))*-1
+        else:
+            desired = Vector2(0,0)
+
+        steering = desired - self.vel 
+        if steering.length() > MAX_FORCE: 
+            steering.scale_to_length(MAX_FORCE)
+        
+        self.apply_force(steering)
+
+    def guard(self):
+        MAX_FORCE = 5
+
+        d = self.first_stop - self.position
+
+        if d.length_squared() == 0:
+            return
+        
+        dist = d.length()
+        if dist < self.STOP_DIST:
+            desired = Vector2(0,0)
+
+        elif dist < self.EYE_SIGHT:
+            # slowing down forces
+            desired = d.normalize() * (MAX_FORCE*(dist/self.EYE_SIGHT))
+        else:
+            desired = d.normalize() * MAX_FORCE
+
+        steering = desired - self.vel 
+        if steering.length() > MAX_FORCE: 
+            steering.scale_to_length(MAX_FORCE)
+        
+        self.apply_force(steering)
+        
+
+
     def apply_force(self, force):
         self.acc += force / self.mass
     
@@ -34,4 +116,9 @@ class Agent:
         self.acc.y = 0
 
     def draw(self,screen):
-        circle(screen, self.cycle_color, self.position, self.radius)
+            
+        circle(screen, (100,100,0), 
+                self.position, self.EYE_SIGHT, width = 1)
+        circle(screen, self.cycle_color, 
+               self.position, self.radius)
+    
--- lab2.py
+++ lab2.py
@@ -18,25 +18,63 @@ class App:
         pygame.time.set_timer(self.CHAGE_DIR, 2000)
         self.running = True
         self.ball = Agent(position = Vector2(screen_width/2, screen_height/2), 
-                          radius= 100, 
-                          color= (255,0,0))
-        self.target = Vector2(0, 0)
-        
+                          radius= 30, 
+                          color= (100,0,0))
+        self.agents = [
+            Agent(position = Vector2(100, screen_height/2), 
+                          radius= 30, 
+                          color= (100,0,0)),
+            Agent(position = Vector2(300, screen_height/2), 
+                          radius= 30, 
+                          color= (100,0,0)),
+            Agent(position = Vector2(500, screen_height/2), 
+                          radius= 30, 
+                          color= (100,0,0))
+        ]
+        self.waypoints = [Vector2(100,0), Vector2(1000, 100), Vector2(0, 800)]
+        self.current_waypoint_numbers = [0, 1, 2]  
+        self.targets = [self.waypoints[self.current_waypoint_numbers[0]],
+                        self.waypoints[self.current_waypoint_numbers[1]],
+                        self.waypoints[self.current_waypoint_numbers[2]],
+        ]   
     
     def handle_input(self):
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-        mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
-        self.target = Vector2(mouse_pos_x, mouse_pos_y)
+        #mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+        #self.target = Vector2(mouse_pos_x, mouse_pos_y)
     
     def update(self, delta_time_ms):
-        self.ball.seek_to(self.target)
-        self.ball.update(delta_time_ms)
+        '''d = self.ball.position - self.target
+        dist = d.length()
+        if dist < 5:
+            self.current_waypoint_number += 1
+            if self. current_waypoint_number <= len(self.waypoints):
+                self.current_waypoint_number = 0
+            
+            self.target = self.waypoints[self.current_waypoint_number]
+        self.ball.arrive_to(self.target)
+        self.ball.update(delta_time_ms)'''
+
+        for i, agent in enumerate(self.agents):
+            d = agent.position - self.targets[i]
+            dist = d.length()
+            if dist < 5:
+                self.current_waypoint_numbers[i] += 1
+                if self. current_waypoint_numbers[i] >= len(self.waypoints):
+                    self.current_waypoint_numbers[i] = 0
+                    print(self.current_waypoint_numbers)
+            
+                self.targets[i] = self.waypoints[self.current_waypoint_numbers[i]]
+            agent.arrive_to(self.targets[i])
+            agent.update(delta_time_ms)
 
     def draw(self):
         self.screen.fill("grey")
-        self.ball.draw(self.screen)
+        #self.ball.draw(self.screen)
+        for agent in self.agents:
+            agent.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
